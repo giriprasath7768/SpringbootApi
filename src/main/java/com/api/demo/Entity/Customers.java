a2f57a4441ddc95da0a7f5e8545cdca71fc3cc59
giriprasath7768 <giriprasat7768@gmail.com>
2022-08-14 10:15:34 +0530

    Customerentity

---
+++ src/main/java/com/api/demo/Entity/Customers.java
@@ -0,0 +1,14 @@
+package com.api.demo.Entity;
+
+public class Customers {
+	private String cusname;
+
+	public String getCusname() {
+		return cusname;
+	}
+
+	public void setCusname(String cusname) {
+		this.cusname = cusname;
+	}
+
+}
